b55df74c8342e9977dc71a9423311c935cab1cf3
Lucas Pizol <lucaspizolfe@gmail.com>
2025-03-11 20:44:02 -0300

    HND-32 Adiciona expandeds na tela inicial e corrige o tamanho das imagens

--- lib/ui/intro/widgets/intro_page.dart
+++ lib/ui/intro/widgets/intro_page.dart
@@ -20,70 +20,78 @@ class _IntroPageState extends State<IntroPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
-        CarouselSlider(
-          options: CarouselOptions(
-            height: 400.0,
-            onPageChanged: (index, reason) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-          ),
-          items: [1, 2, 3].map((i) {
-            return Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/carousel_$i.png'))),
-                );
+        Expanded(
+          flex: 5,
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: double.infinity,
+              viewportFraction: 1.0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
               },
-            );
-          }).toList(),
+            ),
+            items: [1, 2, 3].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                            image:
+                                AssetImage('assets/images/carousel_$i.png'))),
+                  );
+                },
+              );
+            }).toList(),
+          ),
         ),
-        Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-              color: AppColors.primary100,
-              borderRadius: BorderRadius.circular(16)),
-          child: Column(
-            children: [
-              AnimatedSmoothIndicator(
-                activeIndex: _currentIndex,
-                count: 3,
-                effect: ExpandingDotsEffect(
-                  spacing: 8.0,
-                  radius: 40,
-                  dotWidth: 8,
-                  dotHeight: 8,
-                  dotColor: AppColors.grey,
-                  activeDotColor: AppColors.primary400
+        Expanded(
+          flex: 4,
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: AppColors.primary100,
+                borderRadius: BorderRadius.circular(16)),
+            child: Column(
+              children: [
+                AnimatedSmoothIndicator(
+                  activeIndex: _currentIndex,
+                  count: 3,
+                  effect: ExpandingDotsEffect(
+                      spacing: 8.0,
+                      radius: 40,
+                      dotWidth: 8,
+                      dotHeight: 8,
+                      dotColor: AppColors.grey,
+                      activeDotColor: AppColors.primary400),
                 ),
-              ),
-              PrimaryButton(
-                rounded: true,
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/cadastro');
-                },
-                text: 'Criar conta',
-              ),
-              const SizedBox(height: 16),
-              SecondaryButton(
-                rounded: true,
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/login');
-                },
-                text: 'Entrar',
-              ),
-              const SizedBox(height: 16),
-              FlatButton(
-                onPressed: () {},
-                text: 'Entrar com o Google',
-                leftIcon: SvgPicture.asset('images/icons/google.svg'),
-              ),
-            ],
+                PrimaryButton(
+                  rounded: true,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/cadastro');
+                  },
+                  text: 'Criar conta',
+                ),
+                const SizedBox(height: 16),
+                SecondaryButton(
+                  rounded: true,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/login');
+                  },
+                  text: 'Entrar',
+                ),
+                const SizedBox(height: 16),
+                FlatButton(
+                  onPressed: () {},
+                  text: 'Entrar com o Google',
+                  leftIcon: SvgPicture.asset('images/icons/google.svg'),
+                ),
+              ],
+            ),
           ),
         )
       ]),
