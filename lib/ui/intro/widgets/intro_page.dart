c8e77b1f56b16fcdc67feab3955f663fc0bfb218
matessarini <mariana.tessarini@sou.unifeob.edu.br>
2025-03-11 20:42:11 -0300

    HND-32 Desenvolvimento tela Intro

--- lib/ui/intro/widgets/intro_page.dart
+++ lib/ui/intro/widgets/intro_page.dart
@@ -4,6 +4,8 @@ import 'package:app/ui/core/shared/secondary_button.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:app/ui/core/themes/colors.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -14,68 +16,74 @@ class IntroPage extends StatefulWidget {
 
 class _IntroPageState extends State<IntroPage> {
   int _currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
-        Expanded(
-          flex: 5,
-          child: CarouselSlider(
-            options: CarouselOptions(
-              height: double.infinity,
-              viewportFraction: 1.0,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              },
-            ),
-            items: [1, 2, 3].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image:
-                                AssetImage('assets/images/carousel_$i.png'))),
-                  );
-                },
-              );
-            }).toList(),
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 400.0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
           ),
+          items: [1, 2, 3].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/carousel_$i.png'))),
+                );
+              },
+            );
+          }).toList(),
         ),
-        Expanded(
-          flex: 4,
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                PrimaryButton(
-                  rounded: true,
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/cadastro');
-                  },
-                  text: 'Criar conta',
+        Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: AppColors.primary100,
+              borderRadius: BorderRadius.circular(16)),
+          child: Column(
+            children: [
+              AnimatedSmoothIndicator(
+                activeIndex: _currentIndex,
+                count: 3,
+                effect: ExpandingDotsEffect(
+                  spacing: 8.0,
+                  radius: 40,
+                  dotWidth: 8,
+                  dotHeight: 8,
+                  dotColor: AppColors.grey,
+                  activeDotColor: AppColors.primary400
                 ),
-                const SizedBox(height: 16),
-                SecondaryButton(
-                  rounded: true,
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/login');
-                  },
-                  text: 'Entrar',
-                ),
-                const SizedBox(height: 16),
-                FlatButton(
-                  onPressed: () {},
-                  text: 'Entrar com o Google',
-                  leftIcon: SvgPicture.asset('images/icons/google.svg'),
-                ),
-              ],
-            ),
+              ),
+              PrimaryButton(
+                rounded: true,
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/cadastro');
+                },
+                text: 'Criar conta',
+              ),
+              const SizedBox(height: 16),
+              SecondaryButton(
+                rounded: true,
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/login');
+                },
+                text: 'Entrar',
+              ),
+              const SizedBox(height: 16),
+              FlatButton(
+                onPressed: () {},
+                text: 'Entrar com o Google',
+                leftIcon: SvgPicture.asset('images/icons/google.svg'),
+              ),
+            ],
           ),
         )
       ]),
