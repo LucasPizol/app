dc4e9d2aaa7237b0699ac6665b794e9ec42a6892
Lucas Pizol <lucaspizolfe@gmail.com>
2025-03-12 20:45:02 -0300

    HND-37 Melhorias do código na tela inicial

--- lib/ui/intro/widgets/intro_page.dart
+++ lib/ui/intro/widgets/intro_page.dart
@@ -18,91 +18,100 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
-  int _currentIndex = 0;
   final List<String> _titles = [
     'Um tradutor em tempo real conectando pessoas de várias linguas.',
     'Com a inclusão de linguagens de sinais para expandir sua conexão.',
     'Garanta conhecimento em diversas línguas a partir de planos de estudos.',
   ];
 
+  final List<Widget> items =
+      [1, 2, 3].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/carousel_$i.png'),
+                ),
+              ),
+            );
+          },
+        );
+      }).toList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary100,
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  CarouselSlider(
-                    options: CarouselOptions(
-                      height: double.infinity,
-                      autoPlay: true,
-                      viewportFraction: 1.0,
-                      onPageChanged: (index, reason) {
-                        setState(() {
-                          _currentIndex = index;
-                        });
-                      },
-                    ),
-                    items:
-                        [1, 2, 3].map((i) {
-                          return Builder(
-                            builder: (BuildContext context) {
-                              return Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.height,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                      'assets/images/carousel_$i.png',
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          );
-                        }).toList(),
-                  ),
-                  Positioned(
-                    top: 14,
-                    left: 14,
-                    child: Image.asset('assets/images/Logo.png'),
-                  ),
-                ],
-              ),
-            ),
-            FractionalTranslation(
-              translation: Offset(0, -0.025),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
-                decoration: BoxDecoration(
-                  color: AppColors.primary100,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Column(
-                  children: [
-                    AnimatedSmoothIndicator(
-                      activeIndex: _currentIndex,
-                      count: 3,
-                      effect: ExpandingDotsEffect(
-                        spacing: 8.0,
-                        radius: 40,
-                        dotWidth: 8,
-                        dotHeight: 8,
-                        dotColor: AppColors.grey,
-                        activeDotColor: AppColors.primary400,
+        child: Consumer<IntroViewModel>(
+          builder: (context, viewModel, child) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: double.infinity,
+                          autoPlay: true,
+                          viewportFraction: 1.0,
+                          onPageChanged: (index, reason) {
+                            viewModel.onPageChanged(index);
+                          },
+                        ),
+                        items: items,
                       ),
+                      Positioned(
+                        top: 14,
+                        left: 14,
+                        child: Image.asset('assets/images/Logo.png'),
+                      ),
+                    ],
+                  ),
+                ),
+                FractionalTranslation(
+                  translation: Offset(0, -0.025),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
+                    decoration: BoxDecoration(
+                      color: AppColors.primary100,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.black.withAlpha(130),
+                          spreadRadius: 1,
+                          blurRadius: 22,
+                        ),
+                        BoxShadow(
+                          color: AppColors.primary100,
+                          spreadRadius: 1,
+                          offset: Offset(0, 20),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 16),
-                    Consumer<IntroViewModel>(
-                      builder: (context, viewModel, child) {
-                        return Column(
+                    child: Column(
+                      children: [
+                        AnimatedSmoothIndicator(
+                          activeIndex: viewModel.currentIndex,
+                          count: 3,
+                          effect: ExpandingDotsEffect(
+                            spacing: 8.0,
+                            radius: 40,
+                            dotWidth: 8,
+                            dotHeight: 8,
+                            dotColor: AppColors.grey,
+                            activeDotColor: AppColors.primary400,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Column(
                           children: [
                             Text(
-                              _titles[_currentIndex],
+                              _titles[viewModel.currentIndex],
                               style: Font.primary(
                                 fontSize: 15,
                                 fontWeight: FontWeight.w600,
@@ -111,7 +120,7 @@ class _IntroPageState extends State<IntroPage> {
                             ),
                             const SizedBox(height: 16),
                             Text(
-                              'Escolha uma das opções abaixo para continuar.',
+                              'Escolha uma opção para continuar',
                               style: Font.primary(
                                 fontSize: 13,
                                 fontWeight: FontWeight.w300,
@@ -148,14 +157,14 @@ class _IntroPageState extends State<IntroPage> {
                               ),
                             ),
                           ],
-                        );
-                      },
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ],
+              ],
+            );
+          },
         ),
       ),
     );
